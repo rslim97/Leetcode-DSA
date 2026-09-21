933b28469a45170d058b70564cdcc282475fbe1b
rslim97 <reinaldymaslim@gmail.com>
2026-09-21 10:42:24 +0800

    add file

---
+++ 704.py
@@ -0,0 +1,47 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # def helper(low,high):
+        #     while low<=high:
+        #         mid=low+(high-low+1)//2
+        #         if nums[mid]==target:
+        #             return mid
+        #         elif target<nums[mid]:
+        #             high=mid-1
+        #         else:
+        #             low=mid+1
+
+        #     return -1
+        
+        # return helper(0,len(nums)-1)
+
+        l,r=0,len(nums)-1
+        def helper(l,r):
+            if l<=r:
+                mid=l+(r-l+1)//2
+                # print(l,mid,r)
+                if nums[mid]==target:
+                    return mid
+                if target<nums[mid]:
+                    return helper(l,mid-1)
+                else:
+                    return helper(mid+1,r)
+            else:
+                return -1
+        return helper(l,r)    
+
+
+if __name__== '__main__':
+    nums=[-1,0,3,5,9,12]
+    target=9
+    # nums=[-1,0,3,5,9,12]
+    # target=2
+    # nums=[5]
+    # target=5
+    sol=Solution()
+    res=sol.search(nums,target)
+    print(res)
